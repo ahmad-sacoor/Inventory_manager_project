2d3d4eff64d949fca0f0651d3714d282bd6315e4
ahmad-sacoor <ahmadsacoor12@gmail.com>
2026-02-19 15:10:07 +0000

    created project

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $TP04-11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
